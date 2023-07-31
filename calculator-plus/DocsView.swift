9bdf9fd47877e1cbaa634c31849c896a24870329
Cole Smith <colesmith5400@gmail.com>
2023-07-31 15:22:08 -0400

    Fixed docs

--- calculator-plus/DocsView.swift
+++ calculator-plus/DocsView.swift
@@ -10,7 +10,7 @@ struct DocsView: View {
                 x = value
                 b = base
                 
-                root(x, n)
+                root(x,n)
                 nth-root of x
                 """)
             }
